e2f01842f546efee960f37f851d4cf226e344571
Christian Häussler <c-n-i@web.de>
2020-12-03 15:20:02 +0100

    Rais xmrig version to 6.6.2
    Signed-off-by: Christian Häussler <c-n-i@web.de>

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build . --tag cniweb/xmrig:latest
+docker build . --tag cniweb/xmrig:6.6.2
+docker tag cniweb/xmrig:6.6.2 cniweb/xmrig:latest
+docker push cniweb/xmrig:6.6.2
 docker push cniweb/xmrig:latest
