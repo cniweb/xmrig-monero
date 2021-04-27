bddf1bd99322e50e56b34684c89f5cd613d40dcd
Christian Häussler <c-n-i@web.de>
2021-04-27 12:39:58 +0200

    Add ghcr.io Package push
    Signed-off-by: Christian Häussler <c-n-i@web.de>

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 docker build . --tag cniweb/xmrig:6.12.1
 docker tag cniweb/xmrig:6.12.1 cniweb/xmrig:latest
-docker push cniweb/xmrig:6.12.1
-docker push cniweb/xmrig:latest
+docker tag cniweb/xmrig:6.12.1 ghcr.io/cniweb/xmrig:6.12.1
+docker tag cniweb/xmrig:6.12.1 ghcr.io/cniweb/xmrig:latest
+#docker push cniweb/xmrig:6.12.1
+#docker push cniweb/xmrig:latest
+docker push ghcr.io/cniweb/xmrig:6.12.1
+docker push ghcr.io/cniweb/xmrig:latest
