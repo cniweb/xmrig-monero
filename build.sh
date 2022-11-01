421ac1ef60dc928561ff8bc1aa056cb06e3d8d6b
Christian Häussler <c-n-i@web.de>
2022-11-01 01:13:38 +0100

    Bump to xmrig version 6.18.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="6.18.0"
+version="6.18.1"
 image="xmrig"
 docker build . --tag docker.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
