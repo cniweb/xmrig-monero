081d5e501068accb5c5bd1052c1921c72414f94e
Christian Häussler <c-n-i@web.de>
2024-07-04 20:02:13 +0200

    Add no-color parameter

--- start_unmineable.sh
+++ start_unmineable.sh
@@ -2,4 +2,4 @@
 cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
 echo "Start XMRig with Parameters: -a $ALGO -o $POOL_ADDRESS -u LTC:$WALLET_USER.$HOSTNAME#Jumper -p $PASSWORD --threads=$cpu_threads"
 #./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u LTC:LNec6RpZxX6Q1EJYkKjUPBTohM7Ux6uMUy.HOSTNAME#Jumper -p x --threads=4
-./xmrig -a $ALGO -o $POOL_ADDRESS -u LTC:$WALLET_USER.$HOSTNAME#Jumper -p $PASSWORD --threads=$cpu_threads
+./xmrig -a $ALGO -o $POOL_ADDRESS -u LTC:$WALLET_USER.$HOSTNAME#Jumper -p $PASSWORD --threads=$cpu_threads --no-color
