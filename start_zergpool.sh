1ce8fc3f43c482a941a2e24b5f0a98865737fc02
Christian Häussler <c-n-i@web.de>
2025-11-11 13:16:05 +0100

    Update XMRig user parameter format for LTC

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
-echo "Start XMRig with Parameters: -a $ALGO -o $POOL_ADDRESS -u $WALLET_USER -p $PASSWORD,id=$HOSTNAME --threads=$cpu_threads --no-color"
-exec ./xmrig -a "$ALGO" -o "$POOL_ADDRESS" -u "$WALLET_USER" -p "$PASSWORD,id=$HOSTNAME" --threads="$cpu_threads" --no-color
+echo "Start XMRig with Parameters: -a $ALGO -o $POOL_ADDRESS -u LTC:$WALLET_USER.$HOSTNAME#Jumper -p $PASSWORD,id=$HOSTNAME --threads=$cpu_threads --no-color"
+exec ./xmrig -a "$ALGO" -o "$POOL_ADDRESS" -u "LTC:$WALLET_USER.$HOSTNAME#Jumper" -p "$PASSWORD --threads="$cpu_threads" --no-color
